b706b361f7f9da8cd5c676c6b7ce9818ab2deadf
Alisson <alissilva1231@hotmail.com>
2022-04-17 18:29:42 -0300

    feat: add dotenv initialization

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import routes from "./Routes";
+import dotenv from "dotenv";
 import "reflect-metadata";
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
