b134ff20d7ee225b9d2542413126a9820fdfb069
Alisson <alissilva1231@hotmail.com>
2022-04-17 18:46:00 -0300

    feat: implements environmental variables

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,12 @@ import cors from "cors";
 import routes from "./Routes";
 import dotenv from "dotenv";
 import "reflect-metadata";
+import Connection from '../database/connection';
 
 dotenv.config();
 const app = express();
+
+Connection;
 
 app.use(express.json());
 app.use(cors());
