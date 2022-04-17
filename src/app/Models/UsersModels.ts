64723dc96a95df66afbc5916437aac53365288a0
Alisson <alissilva1231@hotmail.com>
2022-04-17 18:51:49 -0300

    feat: create Users models

---
+++ src/app/Models/UsersModels.ts
@@ -0,0 +1,42 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+
+@Entity()
+class UsersModels {
+    @PrimaryColumn({generated: "uuid"})
+    id: String;
+
+    @Column()
+    firstName: String;
+    
+    @Column()
+    lastName: String;
+    
+    @Column()
+    birthday: Date;
+    
+    @Column({
+        enum: ["M", "F"],
+
+    })
+    gender: "M" | "F";
+    
+    @Column()
+    email: String;
+    
+    @Column()
+    passwordHash: String;
+    
+    @Column({
+        default: new Date(Date.now())
+    })
+    createdAt: Date;
+    
+    @Column({
+        default: new Date(Date.now())
+    })
+    updatedAt: Date;
+
+}
+
+export default UsersModels;
