22e024ae3853552681087e6f009c86ed78003d1e
Alisson <alissilva1231@hotmail.com>
2022-05-01 14:03:05 -0300

    feat: increments Controllers

--- src/app/Routes/Users.ts
+++ src/app/Routes/Users.ts
@@ -5,9 +5,9 @@ const users = Router();
 
 const User = new UsersControllers();
 
-users.get("/users/login", () => {});
-users.post("/users/register", () => {});
-users.put("/users/update", () => {});
-users.delete("/users/delete", () => {});
+users.get("/users/login", User.Login);
+users.post("/users/register", User.Register);
+users.put("/users/update", User.Update);
+users.delete("/users/delete", User.Delete);
 
 export default users;
