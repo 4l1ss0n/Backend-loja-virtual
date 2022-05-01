86a92c1e171730d91c9a832f6435ee22c1314137
Alisson <alissilva1231@hotmail.com>
2022-05-01 20:24:17 -0300

    feat: increments middlewares

--- src/app/Routes/Address.ts
+++ src/app/Routes/Address.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import AddressControllers from "../Controllers/AddressControllers";
-import authorization from "../middlewares/authorization";
+import authorization from "../Middlewares/authorization";
 
 const addr = Router();
 
 const Addr = new AddressControllers();
 
 addr.get("/addr", authorization, Addr.Index);
-addr.get("/addr/:id", Addr.Show);
-addr.post("/addr/create", Addr.Create);
-addr.put("/addr/update", Addr.Update);
-addr.delete("/addr/delete", Addr.Delete);
+addr.get("/addr/:id", authorization, Addr.Show);
+addr.post("/addr/create", authorization, Addr.Create);
+addr.put("/addr/update", authorization, Addr.Update);
+addr.delete("/addr/delete", authorization, Addr.Delete);
 
 export default addr;
